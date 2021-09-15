78ebf2d937106b668c8c0ca222d7dceff4e5d2b2
María José Pazmiño <65615849+mjpazmino@users.noreply.github.com>
2021-09-15 17:54:03 -0500

    Update consts.py

--- consts.py
+++ consts.py
@@ -3,11 +3,11 @@ DB_HOST = "localhost"
 DB_PORT = 9042
 
 # Credenciales Meaning Cloud
-MC_LICENSE = 'fd48a107b839f2b3c98fd066070665ff'
+MC_LICENSE = '.......'
 
 
 # Credenciales de Twitter
-API_KEY = 'R2KkNYX8uvE26xezkrGgQ5Xzz'
-API_SECRET_KEY = '6Q0yUnfeuWsuK15IVt5WpSGtpzYgDyDQ1QQZg9kF3PePNdhO0g'
-ACCESS_TOKEN = '2802676408-Qsx7zIiUIQTivydyG1i92hqvXvI3wfX2HJ1rd7F'
-ACCESS_TOKEN_SECRET = 'Lo0MKYcBB8tRnCxfjecsW310Uunq1pW7pTyxg28UYnRIc'
+API_KEY = '......'
+API_SECRET_KEY = '......'
+ACCESS_TOKEN = '......'
+ACCESS_TOKEN_SECRET = '......'
